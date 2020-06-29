df50859bb96b40dab9f606f4d765383bf6f5922c
hoyosayaki <kiya.sayoho@gmail.com>
2020-06-29 11:01:23 +0000

    Bootstrapの導入

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   
   root to: 'home#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  devise_for :users
+  
   resources :users do
   resources :posts
 end
